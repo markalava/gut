3a93f9511cae853df81589c571d5df1b60217b5f
Mark C. Wheldon <wheldon@un.org>
2025-10-08 10:38:07 -0400

    Move 'data_manipulation.R' and 'list_manipulation.R' from 'mut' package.

--- R/functions.R
+++ R/functions.R
@@ -2,7 +2,7 @@
 
 ##' Get default argument values as vector
 ##'
-##' Returns the default values for function arguments as a  vector.
+##' Returns the default values for a function argument as a vector.
 ##'
 ##' @param fn Name of function; passed to \code{\link{match.arg}}.
 ##' @param arg Character; name of argument.
